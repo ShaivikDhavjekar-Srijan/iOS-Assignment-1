de092f4257fefcfc87767da5a9c235d9c74c1f81
Shaivik Dhavjekar <shaivik.dhavjekar@srijan.net>
2022-10-20 20:47:35 +0530

    Fixed Compiler Issue

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Shaivik Dhavjekar on 12/09/22.
+//
+
+import SwiftUI
+
+let productList = ProductDataLoader().productData
+
+struct ContentView: View {
+    var columns = [GridItem(.adaptive(minimum: 160),spacing: 20)]
+    var body: some View {
+        ZStack {
+            Color.cyan.ignoresSafeArea()
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id:\.price) {
+                        product in ProductCard(product: product)
+                    }
+                }.padding()
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()    }
+}
