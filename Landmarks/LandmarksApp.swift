ab9ad2313125cfc084ed5aeef4068f04b7206378
Shaivik Dhavjekar <shaivik.dhavjekar@srijan.net>
2023-05-09 13:38:29 +0530

    Integrated Firebase SDK

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct LandmarksApp: App {
-    var body: some Scene {
-        WindowGroup {
-            SplashScreenView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+          SplashScreenView()
+      }
     }
+  }
 }
+
+/*
+ import SwiftUI
+ 
+ @main
+ struct LandmarksApp: App {
+ var body: some Scene {
+ WindowGroup {
+ SplashScreenView()
+ }
+ }
+ }
+ */
